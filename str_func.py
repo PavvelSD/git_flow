4418cd39cfff4d6db60dbb291a51347b48b3c1f6
Pavel Solodukhin <amba.vl2.0@gmail.com>
2023-11-20 01:00:28 +1000

    edit str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,14 @@
 def first_func():
+    '''
+    принимает на вход строку и возвращает ее со всеми заглавными буквами
+    '''
     user_text = input()
     return user_text.upper()
+
+
+def second_func():
+    '''
+    делает заглавными первые буквы каждого слова в строке
+    '''
+    user_text = input()
+    return user_text.title()
